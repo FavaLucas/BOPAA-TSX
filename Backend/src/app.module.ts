6e73d3852fccf162f10cdcedba06681f1437d062
Lucas Fava <lucasfava86@gmail.com>
2024-11-20 11:00:27 -0300

    2024-11-20 Agrego Database Service

--- Backend/src/app.module.ts
+++ Backend/src/app.module.ts
@@ -10,14 +10,10 @@ import { ConfigModule } from '@nestjs/config';
 import { IndicesModule } from './Entities/Indice/indices.modules';
 import { CronService } from './Services/cronService';
 import { CotizacionIndiceModule } from './Entities/CotizacionIndice/cotizacionIndice.module';
-import { EmpresasController } from './Entities/Empresa/empresas.controller';
-import { CotizacionesController } from './Entities/Cotizacion/cotizaciones.controller';
-import { EmpresasService } from './Entities/Empresa/empresas.services';
-import { CotizacionesService } from './Entities/Cotizacion/cotizaciones.services';
-
+import { DatabaseService } from './Services/Database.services';
 
 @Module({
-  imports: [ 
+  imports: [
     ConfigModule.forRoot({ isGlobal: true }),
     ScheduleModule.forRoot(),
     TypeOrmModule.forRoot({
@@ -40,7 +36,7 @@ import { CotizacionesService } from './Entities/Cotizacion/cotizaciones.services
     CotizacionIndiceModule,
   ],
   controllers: [AppController],
-  providers: [AppService, CronService],
+  providers: [AppService, CronService, DatabaseService],
 })
 export class AppModule {}
 
