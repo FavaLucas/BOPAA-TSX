57a59d7943a01c08ed90b7642c62c479128e19b1
Lucas Fava <lucasfava86@gmail.com>
2024-11-29 09:01:27 -0300

    2024-11-29 Actualizacion estilos de graficos front

--- Frontend/src/app/(pages)/home/page.tsx
+++ Frontend/src/app/(pages)/home/page.tsx
@@ -1,19 +1,25 @@
 'use client';
 import React from 'react';
 
-
 import { Navbar } from '@/app/components/navbar/navbar';
 import BodyEmpresas from '@/app/components/body/bodyEmpresas';
-
+import BodyIndices from '@/app/components/bodyIndices/bodyIndices';
 
 
 const Home = () => {
 
 
   return (
-    <div>
-      <Navbar botonRedireccion={"Indices Bursatiles"} url={"./indiceBursatil"}/>
-      <BodyEmpresas />
+    <div className="min-h-screen border bg-gray-100">
+      <Navbar botonRedireccion={"Indices Bursatiles"} url={"./indiceBursatil"} />
+      <div className="flex flex-col md:flex-row justify-center items-center gap-12 p-8">
+        <div className="w-full md:w-1/2 bg-white p-4 rounded-lg shadow-md">
+          <BodyEmpresas />
+        </div>
+        <div className="w-full md:w-1/2 bg-white p-4 rounded-lg shadow-md">
+          <BodyIndices />
+        </div>
+      </div>
     </div>
   );
 }
