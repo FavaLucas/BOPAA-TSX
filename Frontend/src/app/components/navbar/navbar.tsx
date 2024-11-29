45f29e934449e60660fab48f7c91c53a5311ca2f
Lucas Fava <lucasfava86@gmail.com>
2024-11-29 18:11:38 -0300

    2024-11-29 - Actualizo Front

--- Frontend/src/app/components/navbar/navbar.tsx
+++ Frontend/src/app/components/navbar/navbar.tsx
@@ -1,16 +1,16 @@
 import { i18n } from 'next-i18next';
 import React, { useEffect } from 'react'
 import LanguageSwitcher from '../languageSwitcher/LanguageSwitcher';
-import { useTranslation } from 'react-i18next'; // Importar useTranslation
+import { useTranslation } from 'react-i18next';
 
-export function Navbar({ botonRedireccion, url }: any) {
-  const { t } = useTranslation(); // Usar useTranslation
+export function Navbar({ botonRedireccion }: any) {
+  const { t } = useTranslation(); 
 
   const redireccion = botonRedireccion;
   useEffect(() => { }, [redireccion]);
 
   return (
-    <div className="navbar bg-gray-400 text-white shadow-lg h-24" >
+    <div className="navbar bg-gray-400 text-white shadow-lg h-24 shadow-gray-800" >
       <div className="navbar-start">
         <a className='pl-2'>
           <img src="./images/TSX.svg.png" alt="TSX Logo" className="w-16 h-16" />
@@ -18,7 +18,7 @@ export function Navbar({ botonRedireccion, url }: any) {
       </div>
 
       <div className="navbar-center hidden lg:flex">
-        <h1 className="px-5 py-2 rounded-lg text-center font-bold text-xl tracking-wide bg-gradient-to-r from-blue-500 to-blue-700 text-white">
+        <h1 className="px-5 py-2 rounded-lg text-center font-bold text-xl tracking-wide bg-gradient-to-r from-blue-500 to-blue-700 text-white shadow-lg">
           {t('navbar.title')}
         </h1>
       </div>
